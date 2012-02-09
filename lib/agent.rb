c9b4fd37ffc0c6fd326bf2f0a0fe9e2cf016b59f
Mikael Konutgan <mkonutgan@gmail.com>
2012-02-09 21:15:23 +0100

    Try to implement greed algorithm

--- lib/agent.rb
+++ lib/agent.rb
@@ -7,31 +7,55 @@ class AgentPlayer
   
   def play(gamestate)
     if gamestate[:figure_id] == 1
-      calculate_possible_positions_mrx(gamestate)
+      @@possible_positions_mrx = calculate_possible_positions_mrx(gamestate, @@possible_positions_mrx)
     end
-    # p @@possible_positions_mrx
     gamestate[:routes].sample
+    # play_greedy(gamestate)
   end
   
-  def calculate_possible_positions_mrx(gamestate)
+  def calculate_possible_positions_mrx(gamestate, possible_positions_mrx)
     if gamestate[:position_of_mrx] != 0
-      @@possible_positions_mrx = [gamestate[:position_of_mrx]]
-    else      
+      possible_positions_mrx = [gamestate[:position_of_mrx]]
+    else
       new_positions = []
       ticket = gamestate[:mrx_last_used_ticket]
       board = gamestate[:board]
-      
-      @@possible_positions_mrx.each do |p|
+      possible_positions_mrx.each do |p|
         board.routes_from(p).each do |q|
           if q[:ticket] == ticket
-            if !new_positions.include?(q[:station])
+            unless new_positions.include?(q[:station])
               new_positions << q[:station]
             end
           end
         end
       end
-      @@possible_positions_mrx = new_positions.sort
+      possible_positions_mrx = new_positions.sort
     end
+    return possible_positions_mrx
+  end
+  
+  def play_greedy(gamestate)
+    if gamestate[:turns] < 3
+      return gamestate[:routes].sample
+    end
+    minimal_distance = 100
+    minimal_distance_index = 0
+    min_avg = 100
+    min_avg_index = 0
+    return nil if (gamestate[:routes] == [])
+    
+    gamestate[:routes].each_with_index do |r, i|
+      avg = 0
+      @@possible_positions_mrx.each do |p|
+        avg += gamestate[:board].distance(r[:station], p)
+      end
+      avg = avg / @@possible_positions_mrx.length
+      if avg < min_avg
+        min_avg = avg
+        min_avg_index = i
+      end
+    end
+    return gamestate[:routes][min_avg_index]
   end
   
 end
