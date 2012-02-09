261674f8cf81cd4f6a856f4bda49e2e8ff273c17
Mikael Konutgan <mkonutgan@gmail.com>
2012-02-09 21:14:21 +0100

    Cleanup

--- lib/mrx.rb
+++ lib/mrx.rb
@@ -1,14 +1,13 @@
 
 class MrXPlayer
 
-  def what_move(gamestate)
+  def play_greedy(gamestate)
     gamestate[:board].max_avg_distance(gamestate[:routes], gamestate[:positions_of_agents])
   end
 
   def play(gamestate)
     #For a random move: gamestate[:routes].sample
-    move = what_move(gamestate)
-    move
+    play_greedy(gamestate)
   end
   
 end
