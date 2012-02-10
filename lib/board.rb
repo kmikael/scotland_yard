7f9be4b74dda457d5babcb6c4222d95d08dd5cce
Mikael Konutgan <mkonutgan@gmail.com>
2012-02-10 18:30:17 +0100

    Seperate board and player logic

--- lib/board.rb
+++ lib/board.rb
@@ -1,18 +1,15 @@
 
 require 'yaml'
 
-# The board his a huge matrix there is a route from point X to point Y when the matrix at
+# The board is a huge matrix where there is a route from point X to point Y when the matrix at
 # the position (X, Y) is non-zero. The number determines the way one can go there:
 # 1...taxi, 2...bus, 4...underground and 10...ship
-# So for example one can go from X to Y with bus and taxi, the relevant position in the matrix
+# So for example, if one can go from X to Y with bus and taxi, the relevant position in the matrix
 # will be 1 + 2 = 3.
 
 class Board
   
-  attr_accessor :matrix
-  attr_accessor :distance_matrix
-  
-  # Load the array of arrays (matrix) from a yaml-file.
+  # Load the board- and distance-matrix from a yaml-file.
   def initialize
     @matrix = YAML.load_file('yml/board-matrix.yml')
     @distance_matrix = YAML.load_file('yml/distance-table.yml')
@@ -47,34 +44,6 @@ class Board
   # Returns distance between two stations
   def distance(station1, station2)
     @distance_matrix[station1][station2]
-  end
-
-  # Should return the move with the largest avarage distance
-  def max_avg_distance(h_stations, st_agents)
-    max = 0
-    best = 12 # random number
-    h_stations.each_with_index do |st, index|
-      sta = st[:station]
-      avg = 0
-      st_agents.each do |ag|
-        dis = distance(sta,ag)
-        if ag == sta or dis == 1 # there should be a better way to do that
-          avg = 1
-          break
-        elsif dis > 5 # large distances are not important
-          dis = 4
-          avg += dis
-        else
-          avg += dis
-        end
-      end
-      avg = avg/4
-      if avg > max
-        best = index
-        max = avg
-      end
-    end
-    h_stations[best]
   end
 
 end
